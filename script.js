91984412627a88586ddbcdb59b73868275d5fdb2
Alphonse Nguyen <alphatn23@gmail.com>
2017-06-29 15:15:12 -0700

    Added a collage for each genre button

--- script.js
+++ script.js
@@ -35,46 +35,46 @@ function showGenre(img) {
     var id = $(img).attr('id');
     switch (id) {
         case '28':
-            $('body').css("background-image", 'url("https://images5.alphacoders.com/611/thumb-1920-611946.jpg")')
+            $('body').css("background-image", 'url("http://wallpapercave.com/wp/wp1945909.jpg")')
             break;
         case '35':
-            $('body').css('background-image', 'url("http://images.fanpop.com/images/image_uploads/dodgeball-dodgeball-3A-a-true-underdog-story-200345_900_600.jpg")')
+            $('body').css('background-image', 'url("http://1.bp.blogspot.com/-L-kWcYMBMQ4/UrA6xdXTtdI/AAAAAAAAAEo/_sb66aX2ayM/s1600/Media%2Bcollage.png")')
             break;
         case '27':
-            $('body').css('background-image', 'url("https://images8.alphacoders.com/801/thumb-1920-801032.jpg")')
+            $('body').css('background-image', 'url("https://i0.wp.com/indianghoststories.com/wp-content/uploads/2012/11/Horror-Movies.jpg?resize=1024%2C614")')
             break;
         case '53':
-            $('body').css('background-image', 'url("https://images6.alphacoders.com/524/thumb-1920-524997.jpg")')
+            $('body').css('background-image', 'url("http://www.bang2write.com/wp-content/uploads/2015/11/psych-thriller-collage-paint.png")')
             break;
         case '878':
-            $('body').css('background-image', 'url("http://www.hdwallpapers.in/walls/alien_vs_predator-HD.jpg")')
+            $('body').css('background-image', 'url("http://kosmosaicbooks.com/wp-content/uploads/2011/10/20_Favorite_Sci-Fi_Films.jpg")')
             break;
         case '16':
-            $('body').css('background-image', 'url("http://www.hdwallpapersfreedownload.com/uploads/large/cartoons/disney-toy-story-1-2-3-free-hd-wallpaper.jpg")')
+            $('body').css('background-image', 'url("https://meghanmblogs.files.wordpress.com/2015/03/pvr-disney-fest-collage.jpg?w=982")')
             break;
         case '37':
-            $('body').css('background-image', 'url("http://images4.fanpop.com/image/photos/18000000/Christian-Bale-as-Dan-Evans-3-10-to-yuma-18071755-2560-1700.jpg")')
+            $('body').css('background-image', 'url("http://filmschoolhooligans.com/wp-content/uploads/2015/08/Top-10-Western-Films-800x445.jpg")')
             break;
         case '10751':
-            $('body').css('background-image', 'url("http://images6.fanpop.com/image/photos/36300000/Home-Alone-2-Lost-in-New-York-image-home-alone-2-lost-in-new-york-36366311-1280-688.jpg")')
+            $('body').css('background-image', 'url("https://kassyapple.files.wordpress.com/2013/12/untitled-001.jpg")')
             break;
         case '9648':
-            $('body').css('background-image', 'url("http://theredlist.com/media/database/films/cinema/1990/seven/001-seven-theredlist.jpg")')
+            $('body').css('background-image', 'url("http://topyaps.com/wp-content/uploads/2013/04/enhanced-buzz-17264-1365490707-0.jpg")')
             break;
         case '10749':
-            $('body').css('background-image', 'url("https://umad.com/img/2015/8/the-notebook-desktop-wallpaper-8038-8385-hd-wallpapers.jpg")')
+            $('body').css('background-image', 'url("https://i.ytimg.com/vi/XiLac5mr8pw/maxresdefault.jpg")')
             break;
         case '80':
-            $('body').css('background-image', 'url("http://media5.starkinsider.com/wordpress/wp-content/uploads/2015/03/Goodfellas-Tribeca-Film-Festival-screening.jpg")')
+            $('body').css('background-image', 'url("http://images.amcnetworks.com/sundancechannel.com/wp-content/uploads/2016/08/gomorrah_10-ruthless-movie-mobsters_01_700x384.jpg")')
             break;
         case '18':
-            $('body').css('background-image', 'url("https://s-media-cache-ak0.pinimg.com/originals/33/e0/a5/33e0a567f8328114da0dc27174d28eb1.jpg")')
+            $('body').css('background-image', 'url("http://az616578.vo.msecnd.net/files/2016/01/05/6358755938115513421557804808_movis.jpg")')
             break;
         case '36':
-            $('body').css('background-image', 'url("https://assets.epix.com/41385e6234648f2ccae4151d95773b76.jpg")')
+            $('body').css('background-image', 'url("https://s-media-cache-ak0.pinimg.com/736x/d7/76/66/d77666d383b85e8458fa907af28fd689--the-patriot-movie-mel-gibson.jpg")')
             break;
         case '10752':
-            $('body').css('background-image', 'url("http://cdn26.us1.fansshare.com/photo/platoon/platoon-wallpapers-wallpaper-941689370.jpg")')
+            $('body').css('background-image', 'url("http://www.warbitz.com/wp-content/uploads/2017/04/warmoviesintro.jpg")')
             break;
     }
 }
